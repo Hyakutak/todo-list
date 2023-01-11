888a4c9ce90b99af9e61d78ada89db10b3904a77
Pedro Cruz <santahelia.pedro@gmail.com>
2023-01-10 23:43:59 -0300

    Create header task component

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,10 +1,12 @@
 import { Form } from "../form/Form";
+import { HeaderTask } from '../header/HeaderTask';
 import style from './Content.module.css';
 
 export function Content() {
     return (
         <div className={style.content}>
             <Form />
+            <HeaderTask />
         </div>
     );
 }
--- src/components/header/HeaderTask.tsx
+++ src/components/header/HeaderTask.tsx
@@ -1,3 +1,21 @@
+import styles from './HeaderTask.module.css';
+
 export function HeaderTask() {
-    
+    return (
+        <main className={styles.headerTasks}>
+            <div className={styles.allTasks}>
+                <span className={styles.allTasksText}>Tarefas Criadas</span>
+                <div className={styles.allTasksCounter}>
+                    <span className={styles.allTasksCounterText}>0</span>
+                </div>
+            </div>
+            <div className={styles.countTasksDone}>
+                <span className={styles.countTasksDoneText}>Concluídas</span>
+                <div className={styles.allTasksCounter}>
+                    <span className={styles.allTasksCounterText}>0</span>
+                </div>
+                
+            </div>
+        </main>
+    );
 }
